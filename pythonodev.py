2a1f7dd2215d5e77bdea8d0b536996dcc5fd82ff
beyzacstr <117271127+beyzacstr@users.noreply.github.com>
2023-03-10 17:39:43 +0300

    Create pythonodev.py

---
+++ pythonodev.py
@@ -0,0 +1,19 @@
+# python-1
+*veri türleri
+string : metinsel değerleri ifade eder.
+int : tam sayi değerleri ifade eder.
+double : ondalikli sayilari ifade eder.
+bool: true false değerleri alir.
+list : liste halindedir.
+*Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+metinsel ifadeler string e örnektir
+ne kadar ilerlediğimizi gösteren değerler int e
+*Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+örneğin girişte girilen şifre ve mail doğruysa giriş yapar herhangi biri yanlışsa girmez hata verir
+
+mail = True
+password = True
+if mail == True and password == True:
+    print("successful")
+else:
+    print("unsuccessful")
